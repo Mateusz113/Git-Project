ebf7c2e4874ca04568fded9cd0ff1a312c9dd929
Mateusz Chilicki <mateusz.ch.113@gmail.com>
2025-04-16 16:02:58 +0200

    Update RepositoryDto.java
    Added builder

--- src/main/java/com/mateusz113/git_hub_proj/git_hub_project/model/RepositoryDto.java
+++ src/main/java/com/mateusz113/git_hub_proj/git_hub_project/model/RepositoryDto.java
@@ -1,7 +1,10 @@
 package com.mateusz113.git_hub_proj.git_hub_project.model;
 
+import lombok.Builder;
+
 import java.time.OffsetDateTime;
 
+@Builder
 public record RepositoryDto(
         String fullName,
         String description,
