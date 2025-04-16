de324069dc04e49f84040db91f0cd70b565c2f98
Mateusz Chilicki <mateusz.ch.113@gmail.com>
2025-04-16 15:29:03 +0200

    Update RepositoryController.java
    Added a method to save repository info to local database

--- src/main/java/com/mateusz113/git_hub_proj/git_hub_project/controller/RepositoryController.java
+++ src/main/java/com/mateusz113/git_hub_proj/git_hub_project/controller/RepositoryController.java
@@ -3,9 +3,12 @@ package com.mateusz113.git_hub_proj.git_hub_project.controller;
 import com.mateusz113.git_hub_proj.git_hub_project.model.RepositoryDto;
 import com.mateusz113.git_hub_proj.git_hub_project.service.RepositoryService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -17,5 +20,11 @@ public class RepositoryController {
     @GetMapping("/{owner}/{repository-name}")
     public RepositoryDto getRepositoryData(@PathVariable String owner, @PathVariable("repository-name") String repositoryName) {
         return repositoryService.getRepositoryData(owner, repositoryName);
+    }
+
+    @PostMapping("/{owner}/{repository-name}")
+    @ResponseStatus(HttpStatus.CREATED)
+    public RepositoryDto saveRepositoryData(@PathVariable String owner, @PathVariable("repository-name") String repoName) {
+        return repositoryService.saveRepositoryData(owner, repoName);
     }
 }
