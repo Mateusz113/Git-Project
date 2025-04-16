5467218b16bffd9cd91716d28e1234977dd6cc43
Mateusz Chilicki <mateusz.ch.113@gmail.com>
2025-04-16 16:10:42 +0200

    Update RepositoryRepository.java
    Fixed return type to optional

--- src/main/java/com/mateusz113/git_hub_proj/git_hub_project/repository/RepositoryRepository.java
+++ src/main/java/com/mateusz113/git_hub_proj/git_hub_project/repository/RepositoryRepository.java
@@ -3,8 +3,10 @@ package com.mateusz113.git_hub_proj.git_hub_project.repository;
 import com.mateusz113.git_hub_proj.git_hub_project.model.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface RepositoryRepository extends JpaRepository<Repository, Long> {
-    Repository findByFullName(String fullName);
+    Optional<Repository> findByFullName(String fullName);
 
     boolean existsByFullName(String fullName);
 }
